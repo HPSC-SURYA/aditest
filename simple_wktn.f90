34bbab301d9419861392d69b0c808ee037f3c5fe
Ben Greer <benjamin.greer@colorado.edu>
2012-04-08 13:01:31 -0600

    Added comments explaining the code

--- simple_wktn.f90
+++ simple_wktn.f90
@@ -1,4 +1,11 @@
 
+! the problem with the wakatani method is that it is very abstracted from our problem
+! you have to take adi, isolate the tridi solve, then isolate a single recursion relation
+! (of which there are two), then apply this algorithm
+
+! the purpose of this code is to just code up the recursion relation
+! then we can build on this and have something that is useful to us
+
 program simple_wktn
 
 end program simple_wktn
