2006302a11f542aea5448810df6cecbd85a9eda1
Ben Greer <benjamin.greer@colorado.edu>
2012-04-08 17:02:20 -0600

    Seems to work. Look at for 1D Wakatani example

--- simple_wktn.f90
+++ simple_wktn.f90
@@ -6,6 +6,84 @@
 ! the purpose of this code is to just code up the recursion relation
 ! then we can build on this and have something that is useful to us
 
+
+! w(i) = a(i) * w(i-1) + b(i)
+
 program simple_wktn
+	implicit none
+	include "mpif.h"
+
+	integer myid, numproc, ierr, stat(MPI_STATUS_SIZE)
+	! if you forget that (MPI_..) may Zeus strike you down
+	real*8, dimension(:), allocatable :: w, a, b
+	real*8 corr, acorr
+	integer startindex, indexspan, n, ii, ij
+	parameter (n=16)
+
+	call MPI_Init(ierr)
+	call MPI_Comm_rank(MPI_COMM_WORLD, myid, ierr)
+	call MPI_Comm_size(MPI_COMM_WORLD, numproc, ierr)
+
+	! determine problem size
+	indexspan = n/numproc
+	startindex = myid*indexspan+1
+	! allocate things
+	allocate(w(indexspan))
+	allocate(a(indexspan))
+	allocate(b(indexspan))
+	! pre-compute coefs
+	do ii=1,indexspan
+		a(ii) = 2d0
+		b(ii) = 0d0
+		w(ii) = 0d0
+	enddo
+	if (myid .eq. 0) then
+		b(1) = 1d0
+	endif
+	! for correcting the final data point first
+	acorr = 1d0
+	do ii=1,indexspan
+		acorr = acorr*a(ii)
+	enddo
+
+	call MPI_Barrier(MPI_COMM_WORLD, ierr)
+
+	! Guess Phase
+	w(1) = a(1)*0d0 + b(1) ! just to illustrate
+	do ii=2,indexspan
+		w(ii) = a(ii)*w(ii-1) + b(ii)
+	enddo
+
+	! Propagation Phase
+	if (myid .ne. 0) then
+		call MPI_Recv(corr, 1, MPI_REAL8, myid-1, 0, MPI_COMM_WORLD, stat, ierr)
+		w(indexspan) = w(indexspan) + acorr*corr
+	endif
+	if (myid .ne. numproc-1) then
+		call MPI_Send(w(indexspan), 1, MPI_REAL8, myid+1, 0, MPI_COMM_WORLD, ierr)
+	endif
+
+	! Correction Phase
+	acorr = 1d0
+	do ii=1,indexspan-1
+		acorr = acorr * a(ii)
+		w(ii) = w(ii) + acorr*corr
+	enddo
+
+
+	! output
+	do ii=0,numproc-1
+		if (myid .eq. ii) then
+			write(*,*), "proc", myid
+			write(*,'(4f8.1)'), w
+		endif
+		call MPI_Barrier(MPI_COMM_WORLD, ierr)
+	enddo
+
+	deallocate(w)
+	deallocate(a)
+	deallocate(b)
+
+	call MPI_Finalize(ierr)
 
 end program simple_wktn
