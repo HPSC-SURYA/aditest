54efc412b0f4dd1a0be61295b4bf4cfb98426bc0
Ben Greer <benjamin.greer@colorado.edu>
2012-04-08 23:48:19 -0600

    Not quite working, but getting close

--- tridi_wktn.f90
+++ tridi_wktn.f90
@@ -7,7 +7,7 @@ program tridi_wktn
 	include "mpif.h"
 	
 	integer n, myid, numproc, ierr, stat(MPI_STATUS_SIZE)
-	real*8, dimension(:), allocatable :: a, b, c, d, x
+	real*8, dimension(:), allocatable :: a, b, c, d, x, bp
 	integer indexspan, startindex, ii
 	parameter (n=8)
 
@@ -18,11 +18,12 @@ program tridi_wktn
 	indexspan = n/numproc
 	startindex = myid*indexspan+1
 
-	allocate(a(indexspan))
-	allocate(b(indexspan))
-	allocate(c(indexspan))
-	allocate(d(indexspan))
+	allocate(a(n))
+	allocate(b(n))
+	allocate(c(n))
+	allocate(d(n))
 	allocate(x(indexspan))
+	allocate(bp(n))
 
 	! initialize things
 	a = 1d0
@@ -30,16 +31,19 @@ program tridi_wktn
 	c = 1d0
 	d = -5d-1
 
-	if (numproc .eq. 1) then
-		call solve_tridiag(a,b,c,d,x,n)
-	endif
+	! for testing on 1 processor
+!	if (numproc .eq. 1) then
+!		call solve_tridiag(a,b,c,d,x,n)
+!	endif
+
+	call tridi_condition(a,b,c,d,n,bp)
 
 
 	! output
 	do ii=0,numproc-1
 		if (myid .eq. ii) then
 			write(*,*), "proc", myid
-			write(*,'(8f8.1)'), x
+			write(*,'(4f8.1)'), x
 		endif
 		call MPI_Barrier(MPI_COMM_WORLD, ierr)
 	enddo
@@ -50,16 +54,69 @@ program tridi_wktn
 end program tridi_wktn
 
 
+subroutine tridi_condition(a,b,c,d,n,bp)
+	implicit none
+	integer n, ii
+	real(8), dimension(n) :: a,b,c,d,bp
+
+	bp(1) = b(1)
+	do ii=2,n
+		bp(ii) = b(ii) - (a(ii)/bp(ii-1))*c(ii-1)
+	enddo
+
+end subroutine tridi_condition
+
 ! the magic happens here
-subroutine tridi(a,b,c,d,x,n)
+subroutine tridi(a,b,c,d,x,n,myid,numproc)
 	implicit none
 	include "mpif.h"
-	integer n, i
+	integer n, myid, numproc, ii, ierr, stat(MPI_STATUS_SIZE), req
 	real(8), dimension(n) ::  a,b,c,d ! apparently needs the ::
-	real(8), dimension(n) :: x
+	real(8), dimension(n) :: x, acorr
 	real(8), dimension(n) :: bp, vp
-	real(8) :: m
-	
+	real(8) :: m, corr, sendcorr
+
+	! do first non-wakatani pass
+	bp(1) = b(1)
+	do ii=2,n
+		m = a(ii)/bp(ii-1)
+        bp(ii) = b(ii) - m*c(ii-1)
+	enddo
+
+! Start First Pass
+	! Guess Phase
+	if (myid .eq. 0) then
+		vp(1) = d(1)
+	else
+		vp(1) = 0d0
+	endif
+	acorr(1) = 1d0
+	do ii=2,n
+		m = a(ii)/bp(ii-1)
+		vp(ii) = d(ii) - m*vp(ii-1)
+		acorr(ii) = acorr(ii-1) * (-m) ! prepare correction factor
+	enddo
+
+	! Propagation Phase
+	if (myid .ne. 0) then
+        call MPI_Recv(corr, 1, MPI_REAL8, myid-1, 0, MPI_COMM_WORLD, stat, ierr)
+		vp(n) = vp(n) + acorr(n)*corr
+	endif
+	if (myid .ne. numproc-1) then
+		sendcorr = vp(n)
+	    call MPI_Isend(sendcorr, 1, MPI_REAL8, myid+1, 0, MPI_COMM_WORLD, req, ierr)
+	endif
+
+	! Correction Phase
+!	acorr = 1d0
+!	do ii=1,indexspan-1
+!		acorr = acorr * a(ii)
+!		w(ii) = w(ii) + acorr*corr
+!	enddo
+!	call MPI_Wait(req, stat, ierr)
+
+
+
 end subroutine tridi
 
 ! for reference
